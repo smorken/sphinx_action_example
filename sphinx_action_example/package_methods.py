964a147d690c03a26b0d0e4fbd6c838c7981598d
Scott <scottm361@gmail.com>
2023-01-25 20:53:22 -0800

    add method to read package data

--- sphinx_action_example/package_methods.py
+++ sphinx_action_example/package_methods.py
@@ -1,3 +1,6 @@
+import os
+
+
 def example_function1(a: int, b: float) -> float:
     """Compute the sum of a and b
 
@@ -9,3 +12,13 @@ def example_function1(a: int, b: float) -> float:
         float: a, and b added together
     """
     return a + b
+
+
+def read_package_data() -> str:
+    package_data_file = os.path.join(
+        os.path.dirname(os.path.realpath(__file__)),
+        "package_data",
+        "package_data1.txt",
+    )
+    with open(package_data_file, "r") as fp:
+        return fp.read()
